dbf13d9ef6860b369a0e145a21e633784a2fdd85
justinofimbert <justinofimbert@gmail.com>
2022-02-19 03:08:46 -0300

    finished baseline game, still needs some serious tweaks, also need to work on the css

--- game.js
+++ game.js
@@ -1,4 +1,39 @@
+function game(humanSelection) {
+    const output = document.querySelector("#output");
+    const computerSelection = computerPlay()
+    if (humanSelection === computerSelection) {//.
+        output.textContent = "Draw";
+        return;
+    }
+    if ((humanSelection === "scissors" && computerSelection === "paper") || 
+    (humanSelection === "paper" && computerSelection === "rock") || 
+    (humanSelection === "rock" && computerSelection === "scissors")) {
+        output.textContent = `you win this round! ${humanSelection} beats ${computerSelection}!`;
+        scoring("human");
+        return;
+    }
+    else {
+        output.textContent = `you lose this round! ${computerSelection} beats  ${humanSelection}!`;
+        scoring("computer")
+        return;
+    }
+}
 
+function scoring(winner) {
+    if (winner === "human") {
+        humanScoreNode.textContent = ++humanScore;
+    }
+    else {
+        computerScoreNode.textContent = ++computerScore;
+    }
+    if (humanScore > 4 || computerScore > 4) {
+        computerScoreNode.textContent = 5; computerScore = 0;
+        humanScoreNode.textContent = 5; humanScore = 0;
+        alert("game is over, click another button to start again")
+        computerScoreNode.textContent = 0; humanScoreNode.textContent = 0;
+        return;
+    }
+}
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -12,3 +47,10 @@ function computerPlay() {
         return "scissors"
     }
 }
+const humanScoreNode = document.querySelector("#human-score");
+const computerScoreNode = document.querySelector("#computer-score");
+
+let humanScore = 0; let computerScore = 0;
+
+const btns = Array.from(document.querySelectorAll("button"));
+btns.forEach((humanPlay) => humanPlay.addEventListener("click", function() {game(humanPlay.value)}))
