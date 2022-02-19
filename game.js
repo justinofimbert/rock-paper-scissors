208de3ef1faa52fa8eda2853652e25276eff2996
justinofimbert <justinofimbert@gmail.com>
2022-02-19 00:12:38 -0300

    updated structure of header so player and computer scores are in opposite sides, added main style

--- game.js
+++ game.js
@@ -1,6 +1,4 @@
-game()
 
-console.log()
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -13,58 +11,4 @@ function computerPlay() {
     else {
         return "scissors"
     }
-}
-
-function roundOutcome(playerSelection, computerSelection) {
-
-    if (playerSelection.toLowerCase() === computerSelection)
-    {
-        return "draw"
-    }
-    if (computerSelection === "rock") {
-        if (playerSelection === "scissors") {
-            return "computer wins"
-        }
-        else {
-            return "player wins"
-        }
-    }
-    else if (computerSelection === "paper") {
-        if (playerSelection === "rock") {
-            return "computer wins"
-        }
-        else {
-            return "player wins"
-        }
-    }
-    else if (computerSelection === "scissors") {
-        if (playerSelection === "paper") {
-            return "computer wins"
-        }
-        else {
-            return "player wins"
-        }
-    }
-}
-
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    for (let i = 0; i < 5; i++) {
-        let outcome = roundOutcome((prompt("rock, paper or scissors?").toLowerCase()), computerPlay());
-        if (outcome === "draw") {
-            playerWins = ++playerWins;
-            computerWins = ++computerWins;
-            console.log("draw")
-        }
-        else if (outcome === "player wins") {
-            playerWins = ++playerWins;
-            console.log("player wins")
-        }
-        else {
-            computerWins = ++computerWins;
-            console.log("computer wins")
-        }
-    }
-    console.log(`\nplayer wins: ${playerWins}\ncomputer wins: ${computerWins}`)
 }
